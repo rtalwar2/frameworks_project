8fa75427ab2d3c23a2233a5eada6e90aafcf5832
Arthur Turpyn <arthur.turpyn@ugent.be>
2021-11-11 13:42:55 +0100

    removed import

--- frameworks_project/src/controller/GenreDAO.ts
+++ frameworks_project/src/controller/GenreDAO.ts
@@ -1,6 +1,6 @@
 import {createQueryBuilder, getRepository} from "typeorm";
 import {NextFunction, Request, Response} from "express";
-import {User} from "../entity/User";
+
 import {Genre} from "../entity/Genre";
 
 export class GenreDAO {
